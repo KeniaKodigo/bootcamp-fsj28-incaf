be35ad87d6bd83a83aad41c4f2744c9cf700bd0d
KeniaKodigo <kenia.paz@kodigo.org>
2025-05-06 21:00:53 -0600

    introduccion a javascript

---
+++ javascript/estructuras_control.js
@@ -0,0 +1,11 @@
+
+//estructuras condicionales
+function parImpar(numero){
+    if(numero % 2 == 0){ //true
+        console.log("El numero " + numero + " es par");
+    }else{
+        console.log("El numero " + numero + " es impar");
+    }
+}
+let numero = 105;
+parImpar(numero);
---
+++ javascript/test.js
@@ -0,0 +1,29 @@
+//ejecutando la funcion
+sumar()
+
+//declarando
+function sumar(){
+    let result = 10 + 20;
+    return result;
+}
+
+//funcion de expresion
+const dividir = function(){
+
+}
+
+//funcion renderizan mas rapido
+const multiplicar = () => {
+    let result = 10 * 20;
+    return result;
+}
+
+multiplicar();
+
+PI;
+
+const PI = 3.14;
+
+fullname;
+
+let fullname = "kenia paiz";
