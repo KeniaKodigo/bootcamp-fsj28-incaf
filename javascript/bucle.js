cb66aad4907b0a1746a42340dd9fd09ed050bedc
KeniaKodigo <kenia.paz@kodigo.org>
2025-05-12 20:56:29 -0600

    bucles y metodos de arreglos

--- javascript/bucle.js
+++ javascript/bucle.js
@@ -15,7 +15,6 @@ function tablaMultiplicar(tabla){
     for(let i = 1; i <= 10; i++){
         let results = tabla * i
         console.log(`${tabla} * ${i} = ${results}`);
-
     }
 }
 
@@ -24,4 +23,30 @@ tablaMultiplicar(10);
 
 //[1.60, 1.5,] => desordenada
 //
+//while
+function contarWhile(){
+    let i = 1;
 
+    while(i <= 10){
+        //codigo
+        console.log(`Contador con while ${i}`);
+        i++;
+    }
+
+    // let casado = true;
+    // while(casado){
+    //     //codigoo
+    // }
+}
+
+contarWhile();
+
+function contarDoWhile(){
+    let i = 1
+    do{
+        console.log(`Contando con do while ${i}`)
+        i++
+    }while(i >= 10)
+}
+
+contarDoWhile()
